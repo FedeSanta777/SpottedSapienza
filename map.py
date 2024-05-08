5c7ba6d40f1079f7953da9ac7e8d7a5bad8d3d90
FedeSanta777 <71184687+FedeSanta777@users.noreply.github.com>
2024-05-08 16:05:46 +0200

    Inserimento eventi in database

--- map.py
+++ map.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -25,6 +26,21 @@ def index():
     # Recupera tutti gli eventi dal database
     eventi = Eventi.query.all()
     return render_template('mappa.html', eventi=eventi)
+
+# Gestione della richiesta POST per l'inserimento di un nuovo evento
+@app.route('/submit_event', methods=['POST'])
+def submit_event():
+    if request.method == 'POST':
+        nome_evento = request.form['nome_evento']
+        latitudine = request.form['latitudine']
+        longitudine = request.form['longitudine']
+        data_evento = datetime.strptime(request.form['datetimepicker'], '%Y-%m-%d %H:%M')
+
+        nuovo_evento = Eventi(nome_evento=nome_evento, latitudine=latitudine, longitudine=longitudine, data_evento=data_evento)
+        db.session.add(nuovo_evento)
+        db.session.commit()
+
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
